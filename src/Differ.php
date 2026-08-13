d66c80dec980f7160e6f0d1dfe8827d72a5cb244
XXMargoRitaXX <margarita.amelina2011@mail.ru>
2026-08-13 19:52:30 +0300

    Реализован поиск различий между двумя файлами c вложенными структурами

--- src/Differ.php
+++ src/Differ.php
@@ -2,6 +2,7 @@
 
 namespace Gendiff\Differ;
 
+use function Gendiff\Formatter\makeStylish;
 use function Gendiff\Parsers\parse;
 
 function genDiff(string $filePath1, string $filePath2): string
@@ -9,32 +10,76 @@ function genDiff(string $filePath1, string $filePath2): string
     $data1 = parse($filePath1);
     $data2 = parse($filePath2);
 
-    $data = array_merge($data1, $data2);
-    $sortedData = $data;
-    ksort($sortedData);
+    $data = array_replace_recursive($data1, $data2);
+    $sortedData = ksortRecursive($data);
 
-    $lines = array_reduce(
-        array_keys($sortedData),
-        function (array $acc, mixed $key) use ($data1, $data2): array {
+    $diff = getDiff($sortedData, $data1, $data2);
+
+    return makeStylish($diff);
+}
+
+function getDiff(array $data, array $data1, array $data2): array
+{
+    return array_reduce(
+        array_keys($data),
+        function (array $acc, mixed $key) use ($data, $data1, $data2): array {
             $keyInData1 = array_key_exists($key, $data1);
             $keyInData2 = array_key_exists($key, $data2);
 
+            
+            if ($keyInData1 && $keyInData2 && (is_array($data1[$key]) && is_array($data2[$key]))) {
+                $acc[] = [
+                    'key' => $key,
+                    'status' => 'nested',
+                    'data' => getDiff($data[$key], $data1[$key], $data2[$key]),
+                ];
+                return $acc;
+            }
+        
             if ($keyInData1 && $keyInData2 && $data1[$key] === $data2[$key]) {
-                $acc[] = "  {$key}: {$data1[$key]}";
+                $acc[] = [
+                    'key' => $key,
+                    'status' => 'unchanged',
+                    'data' => $data1[$key],
+                ];
                 return $acc;
             }
 
             if ($keyInData1) {
-                $acc[] = "- {$key}: {$data1[$key]}";
+                $acc[] = [
+                    'key' => $key,
+                    'status' => 'removed',
+                    'data' => $data1[$key],
+                ];
             }
 
             if ($keyInData2) {
-                $acc[] = "+ {$key}: {$data2[$key]}";
+                $acc[] = [
+                    'key' => $key,
+                    'status' => 'added',
+                    'data' => $data2[$key],
+                ];
             }
+
             return $acc;
         },
         []
     );
+}
 
-    return "{\n  " . implode("\n  ", $lines) . "\n}\n";
+function ksortRecursive(mixed $array): mixed
+{
+    if (!is_array($array)) {
+        return $array;
+    }
+
+    $arrayWithSortedChildren = array_map(
+        fn($item) => ksortRecursive($item),
+        $array
+    );
+
+    $sortedArray = $arrayWithSortedChildren;
+    ksort($sortedArray);
+
+    return $sortedArray;
 }
---
+++ src/Formatter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Gendiff\Formatter;
+
+const SPACES_COUNT = 4;
+const REPLACER = ' ';
+
+function makeStylish(array $diff): string
+{
+    $iter = function (mixed $currentValue, int $depth) use (&$iter): string {
+        if (!is_array($currentValue)) {
+            return $currentValue;
+        }
+
+        $currentIndent = str_repeat(REPLACER, SPACES_COUNT * $depth - 2);
+
+        $lines = array_map(
+            function ($key, $value) use ($currentIndent, &$iter, $depth): string {
+                $name = $value['key'] ?? $key;
+                $data = $value['data'] ?? $value;
+                $status = $value['status'] ?? '';
+                $sign = match ($status) {
+                    'added' => '+',
+                    'removed' => '-',
+                    'unchanged', 'nested' => ' ',
+                    default => ' ',
+                };
+                return "{$currentIndent}{$sign} {$name}: {$iter($data, $depth + 1)}"; 
+            },
+            array_keys($currentValue),
+            $currentValue
+        );
+
+        $bracketIndent = str_repeat(REPLACER, SPACES_COUNT * ($depth - 1));
+
+        $result = ['{', ...$lines, "{$bracketIndent}}"];
+
+        return implode("\n", $result);
+    };
+
+    return $iter($diff, 1) . "\n";
+}
