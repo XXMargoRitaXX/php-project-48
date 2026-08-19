dcfd471e8a084d8978cb54188580dc33f6754bee
XXMargoRitaXX <margarita.amelina2011@mail.ru>
2026-08-19 16:24:50 +0300

    Добавлены тесты для обработки исключений

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -9,53 +9,113 @@ use function Gendiff\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
-    #[DataProvider('formatProvider')]
-    public function testGenDiff(
-        string $fileFormat, 
-        string $reportFormat
-    ): void
+    public function testGenDiffDefaultReportFormat(): void
     {
-        $filePath1 = $this->getFixtureFullPath("file1.{$fileFormat}");
-        $filePath2 = $this->getFixtureFullPath("file2.{$fileFormat}");
-        $expected = $this->getFixtureFullPath("{$reportFormat}.txt");
+        $filePath1 = self::getFixtureFullPath('file1.json');
+        $filePath2 = self::getFixtureFullPath('file2.json');
+        $expected = self::getFixtureFullPath('stylish.txt');
+
+        $this->assertStringEqualsFile($expected, genDiff($filePath1, $filePath2));
+    }
+
+    #[DataProvider('dataGenDiff')]
+    public function testGenDiff(
+        string $fileName1,
+        string $fileName2,
+        string $reportFormat,
+    ): void {
+        $filePath1 = self::getFixtureFullPath($fileName1);
+        $filePath2 = self::getFixtureFullPath($fileName2);
+        $expected = self::getFixtureFullPath("{$reportFormat}.txt");
 
         $this->assertStringEqualsFile(
-            $expected, 
+            $expected,
             genDiff($filePath1, $filePath2, $reportFormat)
         );
     }
 
-    public static function formatProvider(): array
+    #[DataProvider('dataGenDiffExceptions')]
+    public function testGenDiffExceptions(
+        string $fileName1,
+        string $fileName2,
+        string $reportFormat,
+        string $expected,
+    ): void {
+        $filePath1 = self::getFixtureFullPath($fileName1);
+        $filePath2 = self::getFixtureFullPath($fileName2);
+
+        $this->expectOutputString($expected);
+        $this->assertNull(genDiff($filePath1, $filePath2, $reportFormat));
+    }
+
+    public static function dataGenDiff(): array
     {
         return [
-            'Two valid JSON files, stylish format' => [
-                'json', 
+            'JSON file format' => [
+                'file1.json',
+                'file2.json',
                 'stylish',
             ],
-            'Two valid YAML files, stylish format' => [
-                'yaml', 
+            'YAML file format (.yaml, .yml)' => [
+                'file1.yaml',
+                'file2.yml',
                 'stylish',
             ],
-            'Two valid JSON files, plain format' => [
-                'json', 
+            'plain report format' => [
+                'file1.json',
+                'file2.json',
                 'plain',
             ],
-            'Two valid YAML files, plain format' => [
-                'yml', 
-                'plain',
-            ],
-            'Two valid JSON files, json format' => [
-                'json', 
+            'json report format' => [
+                'file1.json',
+                'file2.json',
                 'json',
             ],
-            'Two valid YAML files, json format' => [
-                'yaml', 
-                'json',
+            'file name contains Cyrillic characters' => [
+                'файл1.json',
+                'file2.json',
+                'stylish',
             ],
         ];
     }
 
-    public function getFixtureFullPath(string $fixtureName): string
+    public static function dataGenDiffExceptions(): array
+    {
+        $nonExistentFile = 'file3.json';
+        $nonExistentFilePath = self::getFixtureFullPath($nonExistentFile);
+
+        $withoutExtensionFile = 'file1';
+        $withoutExtensionFilePath = self::getFixtureFullPath($withoutExtensionFile);
+
+        return [
+            'non-existent file' => [
+                'file1.json',
+                $nonExistentFile,
+                'stylish',
+                "File '{$nonExistentFilePath}' not found" . PHP_EOL,
+            ],
+            'file without extension' => [
+                $withoutExtensionFile,
+                'file2.json',
+                'stylish',
+                "File '{$withoutExtensionFilePath}' has no extension" . PHP_EOL,
+            ],
+            'unsupported file format' => [
+                'file1.xml',
+                'file2.json',
+                'stylish',
+                "File format '.xml' is not supported" . PHP_EOL,
+            ],
+            'unsupported report format' => [
+                'file1.json',
+                'file2.json',
+                'simple',
+                "Report format 'simple' is not supported" . PHP_EOL,
+            ],
+        ];
+    }
+
+    public static function getFixtureFullPath(string $fixtureName): string
     {
         $parts = [__DIR__, 'fixtures', $fixtureName];
         return realpath(implode('/', $parts));
